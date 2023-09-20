8f76666edf828b0695639cbe02dddaa104f2a88a
Ash Manning <ash@layertwolabs.com>
2023-09-20 22:05:42 +0800

    gui: show plaintext bitnames in my bitnames tab

--- app/gui/my_bitnames.rs
+++ app/gui/my_bitnames.rs
@@ -9,13 +9,14 @@ use crate::app::App;
 pub struct MyBitnames;
 
 impl MyBitnames {
-    pub fn show(&mut self, app: &mut App, ui: &mut egui::Ui) {
+    pub fn show_reservations(&mut self, app: &mut App, ui: &mut egui::Ui) {
         // all bitname reservations
         let bitname_reservations = app
             .utxos
             .values()
             .filter_map(FilledOutput::reservation_data);
-        // split into bitnames for which the names are known or unknown
+        // split into bitname reservations for which the names are known,
+        // or unknown
         let (
             mut known_name_bitname_reservations,
             mut unknown_name_bitname_reservations,
@@ -40,59 +41,91 @@ impl MyBitnames {
         );
         // sort name-unknown bitname reservations by txid
         unknown_name_bitname_reservations.sort_by_key(|(txid, _)| *txid);
-        let mut bitnames: Vec<_> = app
-            .utxos
-            .values()
-            .filter_map(FilledOutput::bitname)
-            .collect();
-        bitnames.sort();
-        let _reservations_response =
-            egui::SidePanel::left("My BitName Reservations")
-                .exact_width(250.)
-                .resizable(false)
-                .show_inside(ui, |ui| {
-                    ui.heading("BitName Reservations");
-                    egui::Grid::new("My BitName Reservations")
-                        .striped(true)
-                        .show(ui, |ui| {
-                            for (txid, commitment, plaintext_name) in
-                                known_name_bitname_reservations
-                            {
-                                let txid = hex::encode(txid.0);
-                                let commitment = hex::encode(commitment);
-                                ui.monospace(format!(
-                                    "plaintext name: {plaintext_name}"
-                                ));
-                                ui.monospace(format!("txid: {txid}"));
-                                ui.monospace(format!(
-                                    "commitment: {commitment}"
-                                ));
-                                ui.end_row()
-                            }
-                            for (txid, commitment) in
-                                unknown_name_bitname_reservations
-                            {
-                                let txid = hex::encode(txid.0);
-                                let commitment = hex::encode(commitment);
-                                ui.monospace(format!("txid: {txid}"));
-                                ui.monospace(format!(
-                                    "commitment: {commitment}"
-                                ));
-                                ui.end_row()
-                            }
-                        });
-                });
-        let _bitnames_response = egui::SidePanel::left("My BitNames")
+        let _response = egui::SidePanel::left("My BitName Reservations")
+            .exact_width(250.)
+            .resizable(false)
+            .show_inside(ui, move |ui| {
+                ui.heading("BitName Reservations");
+                egui::Grid::new("My BitName Reservations")
+                    .striped(true)
+                    .show(ui, |ui| {
+                        for (txid, commitment, plaintext_name) in
+                            known_name_bitname_reservations
+                        {
+                            let txid = hex::encode(txid.0);
+                            let commitment = hex::encode(commitment);
+                            ui.monospace(format!(
+                                "plaintext name: {plaintext_name}"
+                            ));
+                            ui.monospace(format!("txid: {txid}"));
+                            ui.monospace(format!("commitment: {commitment}"));
+                            ui.end_row()
+                        }
+                        for (txid, commitment) in
+                            unknown_name_bitname_reservations
+                        {
+                            let txid = hex::encode(txid.0);
+                            let commitment = hex::encode(commitment);
+                            ui.monospace(format!("txid: {txid}"));
+                            ui.monospace(format!("commitment: {commitment}"));
+                            ui.end_row()
+                        }
+                    });
+            });
+    }
+
+    pub fn show_bitnames(&mut self, app: &mut App, ui: &mut egui::Ui) {
+        // all owned bitnames
+        let bitnames = app.utxos.values().filter_map(FilledOutput::bitname);
+        // split into bitnames for which the names are known or unknown
+        let (mut known_name_bitnames, mut unknown_name_bitnames): (
+            Vec<_>,
+            Vec<_>,
+        ) = bitnames.partition_map(|bitname| {
+            let plain_bitname = app
+                .wallet
+                .get_bitname_plaintext(bitname)
+                .expect("failed to retrieve bitname data");
+            match plain_bitname {
+                Some(plain_bitname) => Either::Left((*bitname, plain_bitname)),
+                None => Either::Right(*bitname),
+            }
+        });
+        // sort name-known bitnames by plain name
+        known_name_bitnames.sort_by(|(_, plain_name_l), (_, plain_name_r)| {
+            plain_name_l.cmp(plain_name_r)
+        });
+        // sort name-unknown bitnames by bitname value
+        unknown_name_bitnames.sort();
+        egui::SidePanel::left("My BitNames")
             .exact_width(250.)
             .resizable(false)
             .show_inside(ui, |ui| {
                 ui.heading("BitNames");
                 egui::Grid::new("My BitNames").striped(true).show(ui, |ui| {
-                    for bitname in bitnames {
-                        ui.monospace(hex::encode(bitname));
+                    for (bitname, plaintext_name) in known_name_bitnames {
+                        ui.monospace(format!(
+                            "plaintext name: {plaintext_name}"
+                        ));
+                        ui.monospace(format!(
+                            "bitname: {}",
+                            hex::encode(bitname)
+                        ));
+                        ui.end_row()
+                    }
+                    for bitname in unknown_name_bitnames {
+                        ui.monospace(format!(
+                            "bitname: {}",
+                            hex::encode(bitname)
+                        ));
                         ui.end_row()
                     }
                 });
             });
+    }
+
+    pub fn show(&mut self, app: &mut App, ui: &mut egui::Ui) {
+        let _reservations_response = self.show_reservations(app, ui);
+        let _bitnames_response = self.show_bitnames(app, ui);
     }
 }
