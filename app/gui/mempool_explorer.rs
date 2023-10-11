999a5059096d873d8bc959c9015edc9f5bce14df
Ash Manning <ash@layertwolabs.com>
2023-10-11 20:00:59 +0800

    clippy fixes

--- app/gui/mempool_explorer.rs
+++ app/gui/mempool_explorer.rs
@@ -15,7 +15,7 @@ pub struct MemPoolExplorer {
 
 impl MemPoolExplorer {
     pub fn show(&mut self, app: &mut App, ui: &mut egui::Ui) {
-        let transactions = app.node.get_all_transactions().unwrap_or(vec![]);
+        let transactions = app.node.get_all_transactions().unwrap_or_default();
         let utxos = app.wallet.get_utxos().unwrap_or_default();
         egui::SidePanel::left("transaction_picker")
             .resizable(false)
