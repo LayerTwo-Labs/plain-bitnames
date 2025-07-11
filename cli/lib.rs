bd39eb2d6cb72ec6c6c909b8662f9dd09c75168c
Ash Manning <ash@layertwolabs.com>
2025-07-11 18:25:55 +0800

    Update deps

--- cli/lib.rs
+++ cli/lib.rs
@@ -477,7 +477,10 @@ impl Cli {
         tracing::info!(%request_id);
         let builder = HttpClientBuilder::default()
             .request_timeout(Duration::from_secs(self.timeout_secs))
-            .set_max_logging_length(1024)
+            .set_rpc_middleware(
+                jsonrpsee::core::middleware::RpcServiceBuilder::new()
+                    .rpc_logger(1024),
+            )
             .set_headers(HeaderMap::from_iter([(
                 http::header::HeaderName::from_static("x-request-id"),
                 http::header::HeaderValue::from_str(&request_id)?,
